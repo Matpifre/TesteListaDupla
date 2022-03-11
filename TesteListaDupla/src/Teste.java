d509863f6f52a7abb894398fb9ad6ad5a245dd07
Mateus Pinho <86007159+Matpifre@users.noreply.github.com>
2022-03-11 12:21:56 -0300

    Minha primeira alteração

---
+++ TesteListaDupla/src/Teste.java
@@ -0,0 +1,16 @@
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		Lista lista = new Lista();
+		
+		lista.inserir("Mateus", 123);
+		lista.inserir("Paulo", 124);
+		lista.inserir("Fagner", 125);
+		lista.inserir("Reinaldo", 126);
+
+		lista.imprimir();
+		
+	}
+
+}
